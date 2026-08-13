49b917007fa44da9e8bf8397676ddb7edaf48dea
Alireza Khoeini <alireza.khoeini@yahoo.com>
2026-08-13 15:45:00 +0300

    [UPDATE]: Test and debug new version of simple attention.

--- simple_attention.py
+++ simple_attention.py
@@ -4,9 +4,9 @@ import torch
 
 class SimpleAttention:
     def __init__(self, d_in, d_out):
-        self.W_q = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
-        self.W_k = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
-        self.W_v = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+        self.W_q = torch.nn.Parameter(torch.rand(d_in, d_out))
+        self.W_k = torch.nn.Parameter(torch.rand(d_in, d_out))
+        self.W_v = torch.nn.Parameter(torch.rand(d_in, d_out))
 
     def forward(self, input):
         query = input @ self.W_q
@@ -15,6 +15,7 @@ class SimpleAttention:
 
         atten_scores = query @ key.T
         atten_weights = torch.softmax(atten_scores / (key.shape[-1] ** 0.5), dim=-1)
+        print("Softmax Normalized Attention weights:\n", atten_weights)
         context_vec = atten_weights @ value
 
         return context_vec
