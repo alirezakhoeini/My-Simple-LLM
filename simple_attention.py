45ff8a7572667b43b17604a885a5d1631f6ebcfb
Alireza Khoeini <alireza.khoeini@yahoo.com>
2026-08-13 15:20:27 +0300

    [ADD]: simple attention class.

---
+++ simple_attention.py
@@ -0,0 +1,20 @@
+
+import torch
+
+
+class SimpleAttention:
+    def __init__(self, d_in, d_out):
+        self.W_q = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+        self.W_k = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+        self.W_v = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+
+    def forward(self, input):
+        query = input @ self.W_q
+        key = input @ self.W_k
+        value = input @ self.W_v
+
+        atten_scores = query @ key.T
+        atten_weights = torch.softmax(atten_scores / (key.shape[-1] ** 0.5), dim=-1)
+        context_vec = atten_weights @ value
+
+        return context_vec
